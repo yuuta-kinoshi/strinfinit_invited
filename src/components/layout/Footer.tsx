8f86bf290c1f637f10a5f4e3640a184a1c53d738
Yuuta Kinoshi <pe8449510@gmail.com>
2024-12-31 16:26:17 -0300

    fix

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Footer = () => {
   return (
     <div className='flex justify-center items-center mx-auto my-0 p-28' style={{background: 'var(--dark)', borderTop: '1px solid var(--primary)', color: 'var(--primary)'}}>
